472a80fb828baf451b1b4eab73200c42d6ddbcc5
Andrew McDonald <andrewmcdonald.dev@gmail.com>
2024-06-06 22:45:29 -0400

    Completely rewrote day 14 solution

--- advent-2023/q14/src/main.rs
+++ advent-2023/q14/src/main.rs
@@ -1,304 +1,97 @@
-use core::fmt;
 use std::{fs, time::Instant};
 
-#[derive(Clone, Debug, PartialEq)]
-struct Grid {
-    grid: Vec<Vec<Space>>,
-}
-
-impl Grid {
-    fn get(&self, point: (usize, usize)) -> &Space {
-        &self.grid[point.1][point.0]
-    }
-    fn iter(&self) -> impl Iterator<Item = &Vec<Space>> {
-        self.grid.iter()
-    }
-    fn set(&mut self, new: Space, point: (usize, usize)) {
-        self.grid[point.1][point.0] = new;
-    }
-    fn height(&self) -> usize {
-        self.grid.len()
-    }
-}
-
-impl fmt::Display for Grid {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for row in self.grid.clone() {
-            for e in row {
-                write!(f, "{}", e)?;
-            }
-            writeln!(f)?;
-        }
-        Ok(())
-    }
-}
-
-#[derive(Clone, Debug, PartialEq)]
-enum Space {
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Tile {
     Round,
     Square,
     Empty,
 }
 
-impl Space {
-    fn cvt(c: char) -> Self {
-        match c {
-            '#' => Self::Square,
-            'O' => Self::Round,
-            '.' => Self::Empty,
-            _ => panic!("Bad character input"),
-        }
-    }
+fn parse(input: &str) -> Vec<Vec<Tile>> {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| match c {
+                    '.' => Tile::Empty,
+                    '#' => Tile::Square,
+                    'O' => Tile::Round,
+                    _ => panic!("at the disco"),
+                })
+                .collect()
+        })
+        .collect()
 }
 
-impl fmt::Display for Space {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let c = match self {
-            Space::Round => 'O',
-            Space::Square => '#',
-            Space::Empty => '.',
-        };
-
-        write!(f, "{}", c)
-    }
-}
-
-fn parse(input: String) -> Grid {
-    Grid {
-        grid: input
-            .lines()
-            .map(|line| line.chars().map(Space::cvt).collect())
-            .collect(),
-    }
-}
-
-fn part_1(mut input: Grid) -> usize {
-    for y in 0..input.grid.len() {
-        for x in 0..input.grid[0].len() {
-            if let Space::Round = input.grid[y][x] {
-                let mut k = y;
-
-                while k > 0 && input.grid[k - 1][x] == Space::Empty {
-                    k -= 1;
+fn slide_north(grid: &mut [Vec<Tile>]) {
+    for col in 0..grid[0].len() {
+        let mut empty_or_round_row = 0;
+        for row in 0..grid.len() {
+            let curr = grid[row][col];
+            match curr {
+                Tile::Square => empty_or_round_row = row + 1,
+                Tile::Round => {
+                    // swap the current tile with the empty_or_round one
+                    let replace_with = std::mem::replace(&mut grid[empty_or_round_row][col], curr);
+                    let _ = std::mem::replace(&mut grid[row][col], replace_with);
+                    empty_or_round_row += 1;
                 }
-
-                input.grid[y][x] = Space::Empty;
-                input.grid[k][x] = Space::Round;
+                Tile::Empty => (),
             }
         }
     }
-
-    calculate_load(&input)
 }
 
-fn calculate_load(tilted: &Grid) -> usize {
-    let height = tilted.height();
-    tilted
-        .iter()
+fn weight(grid: &[Vec<Tile>]) -> usize {
+    grid.iter()
+        .rev()
         .enumerate()
-        .map(|(index, row)| {
-            row.iter()
-                .map(|space| {
-                    if let Space::Round = space {
-                        height - index
-                    } else {
-                        0
-                    }
-                })
-                .sum::<usize>()
+        .map(|(i, row)| {
+            let round_rocks = row.iter().filter(|tile| **tile == Tile::Round).count();
+            round_rocks * (i + 1)
         })
         .sum()
 }
-//
-// fn tilt_north_one(input: &Grid, point: (usize, usize)) -> (usize, usize) {
-//     let mut point = point;
-//     while point.1 != 0 {
-//         if let Space::Empty = input.get((point.0, point.1 - 1)) {
-//             point = (point.0, point.1 - 1);
-//         } else {
-//             break;
-//         }
-//     }
-//     point
-// }
-//
-// fn tilt_west_one(input: &Grid, point: (usize, usize)) -> (usize, usize) {
-//     let mut point = point;
-//     while point.0 != 0 {
-//         if let Space::Empty = input.get((point.0 - 1, point.1)) {
-//             point = (point.0 - 1, point.1);
-//         } else {
-//             break;
-//         }
-//     }
-//     point
-// }
-//
-// fn tilt_south_one(input: &Grid, point: (usize, usize), max_y: usize) -> (usize, usize) {
-//     let mut point = point;
-//     while point.1 != max_y {
-//         if let Space::Empty = input.get((point.0, point.1 + 1)) {
-//             point = (point.0, point.1 + 1);
-//         } else {
-//             break;
-//         }
-//     }
-//     point
-// }
-//
-// fn tilt_east_one(input: &Grid, point: (usize, usize), max_x: usize) -> (usize, usize) {
-//     let mut point = point;
-//     while point.0 != max_x {
-//         if let Space::Empty = input.get((point.0 + 1, point.1)) {
-//             point = (point.0 + 1, point.1);
-//         } else {
-//             break;
-//         }
-//     }
-//     point
-// }
-//
-//
-// fn part_2(input: &Grid, iterations: usize) -> usize {
-//     let mut input = input.clone();
-//     let max_x = input.grid[0].len() - 1;
-//     let max_y = input.grid.len() - 1;
-//     for n in 0..iterations {
-//         // North
-//         let mut north_grid = input.clone();
-//         for (y, list) in input.iter().enumerate() {
-//             for (x, _) in list.iter().enumerate() {
-//                 if let Space::Round = input.get((x, y)) {
-//                     let new = tilt_north_one(&north_grid, (x, y));
-//                     north_grid.set(Space::Round, new);
-//                     if new.0 != x || new.1 != y {
-//                         north_grid.set(Space::Empty, (x, y));
-//                     }
-//                 }
-//             }
-//         }
-//
-//         // West
-//         let mut west_grid = north_grid.clone();
-//         for (y, list) in north_grid.iter().enumerate() {
-//             for (x, _) in list.iter().enumerate() {
-//                 if let Space::Round = north_grid.get((x, y)) {
-//                     let new = tilt_west_one(&west_grid, (x, y));
-//                     west_grid.set(Space::Round, new);
-//                     if new.0 != x || new.1 != y {
-//                         west_grid.set(Space::Empty, (x, y));
-//                     }
-//                 }
-//             }
-//         }
-//
-//         // South
-//         let mut south_grid = west_grid.clone();
-//         for (y, list) in west_grid.iter().enumerate() {
-//             for (x, _) in list.iter().enumerate() {
-//                 if let Space::Round = west_grid.get((x, y)) {
-//                     let new = tilt_south_one(&south_grid, (x, y), max_y);
-//                     south_grid.set(Space::Round, new);
-//                     if new.0 != x || new.1 != y {
-//                         south_grid.set(Space::Empty, (x, y));
-//                     }
-//                 }
-//             }
-//         }
-//
-//         // East
-//         let mut east_grid = south_grid.clone();
-//         for (y, list) in south_grid.iter().enumerate() {
-//             for (x, _) in list.iter().enumerate() {
-//                 if let Space::Round = south_grid.get((x, y)) {
-//                     let new = tilt_east_one(&east_grid, (x, y), max_x);
-//                     east_grid.set(Space::Round, new);
-//                     if new.0 != x || new.1 != y {
-//                         east_grid.set(Space::Empty, (x, y));
-//                     }
-//                 }
-//             }
-//         }
-//         if n % 1000000 == 0 {
-//             println!("Progress: {}%", n % 1000000);
-//         }
-//         input = east_grid.clone();
-//     }
-//
-//     calculate_load(&input)
-// }
 
-fn part_2(mut input: Grid, iterations: usize) -> usize {
-    for n in 0..iterations {
-        // North
-        for y in 0..input.grid.len() {
-            for x in 0..input.grid[0].len() {
-                if let Space::Round = input.grid[y][x] {
-                    let mut k = y;
+// rotate 90 degrees clockwise: (x, y) -> (y, -x)
+fn clockwise(grid: &mut Vec<Vec<Tile>>) {
+    let size = grid.len();
+    let mut rotated = vec![vec![Tile::Empty; size]; size];
+    (0..size).for_each(|row| {
+        (0..size).for_each(|col| {
+            rotated[col][size - 1 - row] = grid[row][col];
+        });
+    });
+    *grid = rotated;
+}
 
-                    while k > 0 && input.grid[k - 1][x] == Space::Empty {
-                        k -= 1;
-                    }
-
-                    input.grid[y][x] = Space::Empty;
-                    input.grid[k][x] = Space::Round;
-                }
-            }
-        }
-
-        // West
-        for y in 0..input.grid.len() {
-            for x in 0..input.grid[0].len() {
-                if let Space::Round = input.grid[y][x] {
-                    let mut k = x;
-
-                    while k > 0 && input.grid[y][k - 1] == Space::Empty {
-                        k -= 1;
-                    }
-
-                    input.grid[y][x] = Space::Empty;
-                    input.grid[y][k] = Space::Round;
-                }
-            }
-        }
-
-        // South
-        for y in 0..input.grid.len() {
-            for x in 0..input.grid[0].len() {
-                if let Space::Round = input.grid[y][x] {
-                    let mut k = y;
-
-                    while k < input.grid.len() - 1 && input.grid[k + 1][x] == Space::Empty {
-                        k += 1;
-                    }
-
-                    input.grid[y][x] = Space::Empty;
-                    input.grid[k][x] = Space::Round;
-                }
-            }
-        }
-
-        // East
-        for y in 0..input.grid.len() {
-            for x in 0..input.grid[0].len() {
-                if let Space::Round = input.grid[y][x] {
-                    let mut k = x;
-
-                    while k < input.grid[0].len() - 1 && input.grid[y][k + 1] == Space::Empty {
-                        k += 1;
-                    }
-
-                    input.grid[y][x] = Space::Empty;
-                    input.grid[y][k] = Space::Round;
-                }
-            }
-        }
-        if n % 1000000 == 0 {
-            println!("Progress: {}%", n % 1000000);
-        }
+fn cycle(grid: &mut Vec<Vec<Tile>>) {
+    for _ in 0..4 {
+        slide_north(grid);
+        clockwise(grid);
     }
+}
 
-    calculate_load(&input)
+fn part_1(mut grid: Vec<Vec<Tile>>) -> usize {
+    slide_north(&mut grid);
+    weight(&grid)
+}
+
+fn part_2(mut grid: Vec<Vec<Tile>>) -> usize {
+    let mut seen = vec![grid.clone()];
+
+    loop {
+        cycle(&mut grid);
+        // check if the cycled map has already been seen
+        if let Some(idx) = seen.iter().position(|x| x == &grid) {
+            // figure out length of cycle (watch out: a cycle might only start after a number of steps)
+            let cycle_len = seen.len() - idx;
+            // use cycle length to figure out the index of the final step in the seen list
+            let final_idx = idx + (1_000_000_000 - idx) % cycle_len;
+            return weight(&seen[final_idx]);
+        }
+        seen.push(grid.clone());
+    }
 }
 
 fn main() {
@@ -308,13 +101,13 @@ fn main() {
     let before = Instant::now();
     println!(
         "Part 1: {:<14} | Elapsed Time: {:.2?}",
-        part_1(parse(input.clone())),
+        part_1(parse(&input)),
         before.elapsed()
     );
     let before = Instant::now();
     println!(
         "Part 2: {:<14} | Elapsed Time: {:.2?}",
-        part_2(parse(input), 1000000000),
+        part_2(parse(&input)),
         before.elapsed()
     );
 }
